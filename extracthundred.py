0d14da72b2523bccbf2ca97b3b250c127ca8511e
saipriyajyothula <saipriyajyothula@gmail.com>
2016-09-25 09:38:17 -0400

    entire dataset

--- extracthundred.py
+++ extracthundred.py
@@ -1,12 +1,12 @@
 import random
-f=open("data100.csv","r")
+f=open("dataall.csv","r")
 data = f.read()
-g=open("data100types.js","w")
+g=open("datatypes.js","w")
 d = data.splitlines()
 for i in range(len(d)):
     d[i] = d[i].split(',')
-g.write("[")
-for i in range(100):
+g.write("var typedata = [")
+for i in range(88888):
     g.write("{lat:"+d[i][1]+",long:"+d[i][2]+",type:'"+d[i][3]+"'}")
     g.write(",\n")
 g.write("]")
